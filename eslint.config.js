c455b4fe3f8b724efe5eb641681f0931083202d8
sarinaa-m <sarin.moayedi@gmail.com>
2025-08-23 16:38:40 +0330

    add eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,27 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+// eslint.config.js
 
-export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+// @ts-check
+import js from "@eslint/js";
+import react from "eslint-plugin-react-x";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config({
+  files: ["**/*.ts", "**/*.tsx"],
+  extends: [
+    js.configs.recommended,
+    tseslint.configs.recommended,
+    react.configs.recommended,
+  ],
+  languageOptions: {
+    parser: tseslint.parser,
+    parserOptions: {
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname,
     },
   },
-])
+  rules: {
+    // Put rules you want to override here
+    "react-x/no-class-component": "warn",
+    "jsx-no-duplicate-props": "warn",
+  },
+});
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,12 +2,12 @@
 import { useState } from 'react'
 
 export default function Counter() {
-  const [count, setCount] = useState(0)
-  return (
-    <div>
-      <h2 aria-label="count">{count}</h2>
-      <button onClick={() => setCount(c => c + 1)}>Increase</button>
-      <button onClick={() => setCount(c => c - 1)}>Decrease</button>
-    </div>
-  )
+    const [count, setCount] = useState(0)
+    return (
+        <div>
+            <h2 aria-label="count">{count}</h2>
+            <button onClick={() => setCount(c => c + 1)}>Increase</button>
+            <button onClick={() => setCount(c => c - 1)}>Decrease</button>
+        </div>
+    )
 }
